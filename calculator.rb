2c5fafbdcd70a887a2f730e6b3bc2eaa58143994
Anne Nardolilli <anardolilli@gmail.com>
2020-08-26 14:29:13 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -3,3 +3,6 @@ second_number = 2.to_f
 
 sum = first_number + second_number 
 difference = first_number - second_number 
+product = first_number * second_number
+quotient = first_number / second_number
+
